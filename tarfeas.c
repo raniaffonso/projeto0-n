663163f07464cd432cd1f9ff6ddf85e1674531b9
raniaffonso <161986908+raniaffonso@users.noreply.github.com>
2024-03-19 23:43:22 +0000

    add tarefas.c

---
+++ tarfeas.c
@@ -0,0 +1,23 @@
+#include "tarefas.h"
+#include <stdio.h>
+
+int criar (Tarefa tarefas[], int *pos){
+  printf("Criar tarefa\n");
+  return 0;
+}
+
+int deletar (Tarefa tarefas[], int *pos){
+  printf("deletar tarefa\n");
+  return 0;
+}
+
+int listar (Tarefa tarefas[], int *pos){
+  printf("Listar tarefa\n");
+  return 0;
+}
+
+int criarTarefa(Tarefa tarefas[], int *pos){}
+int deletarTarefa(Tarefa tarefas[], int* pos){}
+int listarTarefa(Tarefa tarefas[], int pos){}
+int salvarTarefa(Tarefa tarefas[],int total, int pos){}
+int carregarTarefa(Tarefa tarefas[],int total, int pos){}
